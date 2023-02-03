c48880332e85b067d9144311b1cde6bd116309a1
Slin Lee <slin@slinlee.com>
2023-02-03 11:08:21 -0800

    Set up venv support in nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A basic flake with a shell";
+  description = "Basic Python Dev env";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -11,11 +11,24 @@
       {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.bashInteractive
             pkgs.python310Packages.pip
+            pkgs.python310
+            pkgs.python310Packages.venvShellHook
+            pkgs.autoPatchelfHook
             pkgs.postgresql
             pkgs.heroku
           ];
+
+          venvDir = "./venv";
+
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+            pip install -U pip
+            pip install -r requirements.txt
+            autoPatchelf ./venv
+          '';
+            
+
         };
       });
 }
