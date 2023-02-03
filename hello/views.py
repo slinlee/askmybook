2ebc00515fbce54ce3900d359fcc68d2ec7ef282
Slin Lee <slin@slinlee.com>
2023-02-03 14:12:12 -0800

    Update default question

--- hello/views.py
+++ hello/views.py
@@ -143,7 +143,7 @@ def answer_query_with_context(
     return response["choices"][0]["text"].strip(" \n"), context
 
 def index(request):
-    return render(request, "index.html", { "default_question": "What is The Minimalist Entrepreneur about?" })
+    return render(request, "index.html", { "default_question": "What is Rivendell Bicycles about?" })
 
 @csrf_exempt
 def ask(request):
